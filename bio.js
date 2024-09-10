22f7bc469086185fa650636802f325d99e9ebaa4
nataleesama <144615407+nataleesama@users.noreply.github.com>
2024-09-10 12:13:56 -0400

    Add files via upload

---
+++ bio.js
@@ -0,0 +1,17 @@
+var loop = 0;
+var images = ['mazzy.jpeg', 'mom.jpeg', 'pump.jpg', 'dad.png', 'nick.jpeg', 'girls.png'];
+var names = ["Mazzy", "Mom", "Pumpkin", "Dad", "Nick", "Girls"];
+
+function loopPics(){
+    setTimeout(function(){
+        document.getElementById("imgs").src = images[loop];
+        document.getElementById("imgs").alt = names[loop];
+        if(loop < images.length - 1){
+            loop += 1;
+        }else{
+            loop = 0;
+        }
+    }, 2500);
+}
+
+
